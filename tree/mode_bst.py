58038d69c33cf6205e8c7c7ee64f3d65258845dd
virinchi <rangerdelli@gmail.com>
2023-04-15 15:19:32 +0530

    added a easy tree prob

---
+++ tree/mode_bst.py
@@ -0,0 +1,34 @@
+# Definition for a binary tree node.
+from typing import Optional,List
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+class Solution:
+    def findMode(self, root: Optional[TreeNode]) -> List[int]:
+        node_map = {}
+        self.findModeHelper(root, node_map)
+        maxi = max(node_map.values())
+        sol = []
+        for k,v in node_map.items():
+            if v==maxi:
+                sol.append(k)
+        return sol
+    
+    def findModeHelper(self, node, node_map):
+        if node==None:
+            return node_map
+
+        node_map[node.val] = node_map.get(node.val, 0) + 1
+
+        self.findModeHelper(node.left, node_map)
+        self.findModeHelper(node.right, node_map)
+
+        return node_map
+
+"""
+Approach:
+We recursively travel the tree and update hash_map with freq,
+and travel hash_map to find all modes
+"""
